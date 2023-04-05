dad522e6eeb70c16e2b1c29388eefc8d65311242
Camelia03 <camelia_weber@yahoo.com>
2023-04-05 17:23:48 +0000

    Generate empty board state

--- run.py
+++ run.py
@@ -6,3 +6,22 @@
 name_str = input("Enter your name:\n")
 print(f"Welcome to Battleships Game, {name_str}!")
 
+class GameBoard:
+    def __init__(self) -> None:
+        self.board_state = []
+
+
+        for i in range(4):
+            cells = []
+            for j in range(4):
+                cells.append(" ")
+            self.board_state.append(cells) 
+
+        print(self.board_state)
+
+board = GameBoard()
+
+
+
+
+        
