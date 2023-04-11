167dcb5efcfd0c9d43e512d53d8cd9f15d038cb4
Camelia03 <camelia_weber@yahoo.com>
2023-04-11 10:41:37 +0000

    Create method for printing the board

--- run.py
+++ run.py
@@ -3,24 +3,20 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 
-#name_str = input("Enter your name:\n")
-#print(f"Welcome to Battleships Game, {name_str}!")
+# name_str = input("Enter your name:\n")
+# print(f"Welcome to Battleships Game, {name_str}!")
 
 class GameBoard:
     def __init__(self) -> None:
         self.board_state = [[" " for i in range(4)] for i in range(4)]
 
     def pretty_print(self):
-       for row in self.board_state:
-        for cell in row:
-            print("|_", end="")
-        print("|")
+        for i, row in enumerate(self.board_state):
+            print(i, end=": ")
+            for cell in row:
+                print("| ", end="")
+            print("|")
 
 
 board = GameBoard()
 board.pretty_print()
-
-
-
-
-        
